3cbb45f366efa3224c7c6e66177f71344e27e4f3
Strahinja <djuricastrahinja@gmail.com>
2019-11-11 17:25:00 +0100

    Cha ge key_mod = key % 26

--- lib/rot.rb
+++ lib/rot.rb
@@ -3,26 +3,26 @@
 class Rot
   def initialize(sentence)
     @sentence = sentence
+
   end
 
   def encrypt(key=0)
     coded = ""
-    key_mod = key % 25
+    key_mod = key % 26
     @sentence.each_char do |letter|
+
       if (65..90).include? letter.ord
         new = (letter.ord + key_mod)
         if new > 90
         new = new % 90 + 65
         end
         coded +=  new.chr
-
       elsif (97..122).include? letter.ord
         new = (letter.ord + key_mod)
         if new > 122
         new = new % 122 + 97
         end
         coded +=  new.chr
-        
       else
         coded += letter
       end
