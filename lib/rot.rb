70b909a7e9f897c061f2e72a3a7054c116b18446
Strahinja <djuricastrahinja@gmail.com>
2019-11-11 16:48:01 +0100

    Remove decrypt function

--- lib/rot.rb
+++ lib/rot.rb
@@ -27,34 +27,4 @@ class Rot
     end
     coded
   end
-
-  def decrypt(key=0)
-    charListSmall = ("a".."z").to_a
-    charListCapitalize = ("A".."Z").to_a
-    decoded = ""
-
-    if key == 0
-      return @sentence
-    else
-      @sentence.each_char do |i|
-        if [" ", ",", ".", ":","!", "?"].include? i
-          decoded += i
-        elsif
-
-          index = charListSmall.find_index(i)
-          newIndex = (index - key) % charListSmall.length
-          decoded += charListSmall[newIndex]
-
-        else
-          index = charListCapitalize.find_index(i)
-          newIndex = (index - key) % charListCapitalize.length
-          decoded += charListCapitalize[newIndex]
-        end
-
-      end
-      decoded
-    end
-  end
 end
-sample = Rot.new ("Abc")
-puts sample.encrypt(52)
