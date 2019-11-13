65a988a3c6158d3ab088bb6beecbe6071cb5815e
Strahinja <djuricastrahinja@gmail.com>
2019-11-13 14:57:09 +0100

    Change ranges

--- lib/rot.rb
+++ lib/rot.rb
@@ -8,13 +8,13 @@ class Rot
     key_mod = key % 26
     @sentence.each_char do |letter|
 
-      if (65..90).include? letter.ord
+      if ("A".."Z").include? letter
         coded_char = letter.ord + key_mod
         if coded_char > 90
           coded_char = coded_char % 90 + 64
         end
         coded +=  coded_char.chr
-      elsif (97..122).include? letter.ord
+      elsif ("a".."z").include? letter
         coded_char = letter.ord + key_mod
         if coded_char > 122
           coded_char = coded_char % 122 + 96
